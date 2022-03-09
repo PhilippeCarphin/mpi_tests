2a47f53cb990451320027a902fede79b9ec66f19
Philippe Carphin <philippe.carphin@polymtl.ca>
2022-03-09 11:16:14 -0500

    Clean version

--- scatter_gather/main.c
+++ scatter_gather/main.c
@@ -1,8 +1,9 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-const int part_size = 3;
+const int part_size = 100;
 
 typedef struct MPI_Helper {
     int mpi_size;
@@ -18,29 +19,74 @@ typedef struct MPI_Helper {
 
 } MPI_Helper_t;
 
-int mpi_helper_init(MPI_Helper_t *mpi_helper)
+int mpi_helper_init_data_in(MPI_Helper_t *h){
+
+    if(h->mpi_rank == 0){
+        for(int chunk_id = 0; chunk_id < h->mpi_size; chunk_id++){
+            int chunk_start = chunk_id * h->data_part_size;
+            int chunk_end = (chunk_id+1) * h->data_part_size;
+            for(int j = chunk_start ; j < chunk_end ; j++){
+                h->data_in[j] = chunk_id;
+            }
+        }
+    }
+    return 0;
+}
+
+int mpi_helper_print_data_out(MPI_Helper_t *h){
+    if(h->mpi_rank == 0){
+        for(int chunk_id = 0; chunk_id < h->mpi_size; chunk_id++){
+            int chunk_start = chunk_id * h->data_part_size;
+            int chunk_end = (chunk_id+1) * h->data_part_size;
+            for(int j = chunk_start; j < chunk_end; j++){
+                int expected = chunk_id * chunk_id;
+                if(h->data_out[j] != expected) {
+                    fprintf(stderr, "h->data_out[%d] = %d different from expected %d\n", j, h->data_out[j], expected);
+                }
+                fprintf(stderr, "h->data_out[%d] = %d\n", j, h->data_out[j]);
+            }
+        }
+    }
+    return 0;
+}
+
+int mpi_helper_process_data_part(MPI_Helper_t *h){
+    for(int i = 0; i < part_size; i++){
+        if(h->data_part_in[i] != h->mpi_rank){
+            fprintf(stderr, "[Rank %d] Unexpected data_in[%d] : %d\n", h->mpi_rank, i, h->data_part_in[i]);
+        }
+    }
+
+    // The process does something to its chunk of data
+    for(int i = 0; i < part_size; i++){
+        h->data_part_out[i] = h->data_part_in[i] * h->mpi_rank;
+    }
+    return 0;
+}
+
+int mpi_helper_init(MPI_Helper_t *h)
 {
-    MPI_Comm_size(MPI_COMM_WORLD, &mpi_helper->mpi_size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_helper->mpi_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &h->mpi_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &h->mpi_rank);
 
-    mpi_helper->data_size = part_size * mpi_helper->mpi_size;
-    mpi_helper->data_part_size = part_size;
+    h->data_size = part_size * h->mpi_size;
+    h->data_part_size = part_size;
 
-    if(mpi_helper->mpi_rank == 0){
-        mpi_helper->data_in  = malloc(mpi_helper->data_size * sizeof(*(mpi_helper->data_in)));
-        mpi_helper->data_out = malloc(mpi_helper->data_size * sizeof(*(mpi_helper->data_out)));
+    if(h->mpi_rank == 0){
+        h->data_in  = malloc(h->data_size * sizeof(*(h->data_in)));
+        h->data_out = malloc(h->data_size * sizeof(*(h->data_out)));
     } else {
-        mpi_helper->data_in = NULL;
-        mpi_helper->data_out = NULL;
+        h->data_in = NULL;
+        h->data_out = NULL;
     }
 
-    mpi_helper->data_part_in = malloc(mpi_helper->data_part_size * sizeof(*(mpi_helper->data_in)));
-    mpi_helper->data_part_out = malloc(mpi_helper->data_part_size * sizeof(*(mpi_helper->data_out)));
+    h->data_part_in = malloc(h->data_part_size * sizeof(*(h->data_in)));
+    h->data_part_out = malloc(h->data_part_size * sizeof(*(h->data_out)));
 
-    if(mpi_helper->mpi_rank == 0){
-        fprintf(stderr, "world_size = %d\n", mpi_helper->mpi_size);
+    if(h->mpi_rank == 0){
+        fprintf(stderr, "world_size = %d\n", h->mpi_size);
     }
-    fprintf(stderr, "world_rank = %d\n", mpi_helper->mpi_rank);
+    fprintf(stderr, "world_rank = %d\n", h->mpi_rank);
 
     return 0;
 }
@@ -48,47 +94,50 @@ int mpi_helper_init(MPI_Helper_t *mpi_helper)
 int main(int argc, char **argv)
 {
     MPI_Init(&argc, &argv);
-    MPI_Helper_t mpi_helper;
-    mpi_helper_init(&mpi_helper);
+    MPI_Helper_t h;
+    mpi_helper_init(&h);
 
     // Root process initializes data
-    if(mpi_helper.mpi_rank == 0){
-        for(int i = 0; i < mpi_helper.data_size; i++){
-            mpi_helper.data_in[i] = (i * mpi_helper.mpi_size)/mpi_helper.data_size;
-        }
+    mpi_helper_init_data_in(&h);
 
-        for(int i = 0; i < mpi_helper.data_size; i++){
-            fprintf(stderr, "mpi_helper.data_in[%d] = %d\n", i, mpi_helper.data_in[i]);
-        }
-    }
-
+    // Each process gets a different chunk of data_in sent by the root process
+    // to its data_part_in.  Note that the root process also sends itself a
+    // chunk of data.
     MPI_Scatter(
-        mpi_helper.data_in,      mpi_helper.data_part_size, MPI_INT,
-        mpi_helper.data_part_in, mpi_helper.data_part_size, MPI_INT,
+        h.data_in,      h.data_part_size, MPI_INT,
+        h.data_part_in, h.data_part_size, MPI_INT,
         0, // root
         MPI_COMM_WORLD
     );
+    // NOTE: The total data size is implicit from the part size and the number
+    // of involved processes
+    // for(int dest_rank = 0; dest_rank < h.mpi_size; dest_rank++){
+    //      int send_start = &(h.data_in[dest_rank * h.data_part_size]);
+    //      MPI_Send(
+    //          send_start, h.data_part_size, MPI_INT,
+    //          dest_rank, // destination
+    //          0, // Tag (irrelevant)
+    //          MPI_WORLD_COMM
+    //      )
+    //  }
 
-    for(int i = 0; i < part_size; i++){
-        // if(mpi_helper.data_in[i] != mpi_helper.mpi_rank){
-        //     fprintf(stderr, "[Rank %d] Unexpected data_in[%d] : %d\n", mpi_helper.mpi_rank, i, mpi_helper.data_in[i]);
-        // }
-        mpi_helper.data_part_out[i] = mpi_helper.data_part_in[i] * mpi_helper.mpi_rank;
-    }
+    // Now each process has a data_part_in whose values should all be equal
+    // to its rank
+    mpi_helper_process_data_part(&h);
 
+    // Each process will send its data_part_out to data_out on the root process
+    // Note that the root process sends its data_part_out to itself.
     MPI_Gather(
-        mpi_helper.data_part_out, mpi_helper.data_part_size, MPI_INT,
-        mpi_helper.data_out,      mpi_helper.data_part_size, MPI_INT,
+        h.data_part_out, h.data_part_size, MPI_INT,
+        h.data_out,      h.data_part_size, MPI_INT,
         0, // root
         MPI_COMM_WORLD
     );
 
-    if(mpi_helper.mpi_rank == 0){
-        for(int i = 0; i < mpi_helper.data_size; i++){
-            fprintf(stderr, "mpi_helper.data_out[%d] = %d\n", i, mpi_helper.data_out[i]);
-        }
-    }
+    // The data should be reassembled in order it was sent.
+    mpi_helper_print_data_out(&h);
 
+    // usleep(10);
     MPI_Barrier(MPI_COMM_WORLD);
 
     MPI_Finalize();
